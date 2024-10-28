357a7a36a07b02f8646f8ac90a11f6e9c3fe47a2
Alberto Blazquez <ablazquezrod@gmail.com>
2024-10-28 12:34:32 +0100

    Replace Vite's native CSS build with LightningCSS

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import browserslist from 'browserslist';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { browserslistToTargets } from 'lightningcss';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./vitest.setup.ts'],
-  },
   base: '',
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'assets/'), // DO NOT remove this line or assets won't be included in the build
-    },
-  },
   build: {
+    cssMinify: 'lightningcss',
     target: browserslistToEsbuild(),
     rollupOptions: {
       logLevel: 'info',
     },
   },
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist("> 0.25%, not dead")),
+    }
+  },
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'assets/'), // DO NOT remove this line or assets won't be included in the build
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
+  },
 });
