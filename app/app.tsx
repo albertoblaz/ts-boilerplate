ba77f7ca476cc6d88e3e25b503e52ce4125c04a0
Alberto Blazquez <ablazquezrod@gmail.com>
2024-10-28 12:32:01 +0100

    Include Tailwind outputted CSS

--- app/app.tsx
+++ app/app.tsx
@@ -2,9 +2,10 @@ import { Icon } from "@app/components/icon/icon";
 
 export function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <Icon name="hola"></Icon>
+    <div className="m-4 flex h-screen w-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Hello, World!</h1>
+      <h2 className="text-xl">Welcome to <pre className="inline font-mono font-bold">ts-boilerplate</pre></h2>
+      <Icon name="hola" />
     </div>
   );
 }
